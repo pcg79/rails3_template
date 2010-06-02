9b67c06c4f9f35ab7a5688512805e2236cf4878e
Rob Christie <robchristie@gmail.com>
2010-06-02 16:14:45 -0400

    Subdividing files.

---
+++ setup_testing.rb
@@ -0,0 +1,22 @@
+
+
+create_file 'test/test_helper.rb', <<-TESTHELPER
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+Bundler.require(:shoulda)
+require 'shoulda'
+
+
+
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
+TESTHELPER
