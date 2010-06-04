faac11413b97df2af8854f211d0ddf611c038967
Rob Christie <robchristie@gmail.com>
2010-06-04 10:03:28 -0400

    changed ordering so that capify is run after bundle.

--- template_cap.rb
+++ template_cap.rb
@@ -3,6 +3,6 @@
 #----------------------------------------------------------------------------
 if yes?('Use Capistrano for the app? (yes/no)')
   gem 'capistrano'
-  capify!
   run "bundle install"
+  capify!
 end
